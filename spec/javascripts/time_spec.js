15326f4f1338ae935a4bc5be25e0405b8366dd2b
Nico Kruger <nico.kruger@gmail.com>
2011-11-30 09:36:15 +0200

    Time extraction shouldn't format to string already.
    It should only happen later, in the countdown itself.

--- spec/javascripts/time_spec.js
+++ spec/javascripts/time_spec.js
@@ -6,28 +6,28 @@ describe("Time conversion functions", function() {
         var d1 = new Date(2012, 12, 1);
         var d2 = new Date(2012, 12, 2);
         expect(convertTime(d1.getTime(), d2.getTime())).toEqual({
-            days:"001",
-            hours:"00",
-            minutes:"00",
-            seconds:"00"
+            days:1,
+            hours:0,
+            minutes:0,
+            seconds:0
         });
         
         d1 = new Date(2011, 12, 1);
         d2 = new Date(2012, 12, 1);
         expect(convertTime(d1.getTime(), d2.getTime())).toEqual({
-            days:"366",
-            hours:"00",
-            minutes:"00",
-            seconds:"00"
+            days:366,
+            hours:0,
+            minutes:0,
+            seconds:0
         });
         
         d1 = new Date(2011, 12, 1, 1);
         d2 = new Date(2012, 12, 1, 2);
         expect(convertTime(d1.getTime(), d2.getTime())).toEqual({
-            days:"366",
-            hours:"01",
-            minutes:"00",
-            seconds:"00"
+            days:366,
+            hours:1,
+            minutes:0,
+            seconds:0
         });
     });
 
